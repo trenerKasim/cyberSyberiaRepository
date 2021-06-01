4b0038760af17bfdd5a593ac6cdfd11fa2b3583d
unknown <jasinski9948@gmail.com>
2021-06-01 18:03:09 +0200

    initial commit
    initial commit xD

---
+++ project/Assets/scripts/mainMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuManager : MonoBehaviour
+{
+	public void playButton()
+	{
+		//
+	}
+
+	public void creditsButton()
+	{
+		//
+	}
+
+    public void exitButton()
+	{
+		Application.Quit();
+	}
+}
